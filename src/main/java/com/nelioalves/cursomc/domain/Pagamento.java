9ff0c113df15296360cdf3cec06e3fc85688c17b
Cleiton <cleitontexas@gmail.com>
2018-12-28 14:56:04 -0200

    ajuste pagamento e pedido um para um OneToOne

--- src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -3,6 +3,10 @@ package com.nelioalves.cursomc.domain;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
@@ -11,9 +15,13 @@ public class Pagamento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Integer id;
 	private EstadoPagamento estado;
 	
+	@OneToOne // garante que o pedido do pagamento seja o mesmo id do pagamemento 
+	@JoinColumn(name="pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
