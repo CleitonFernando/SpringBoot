8e77911a379c86a585dc9d02fa5c0b921e38f7ec
Cleiton <cleitontexas@gmail.com>
2019-01-04 13:49:14 -0200

    deletando clientes com metodo delete

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -30,7 +31,7 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer Tipo;
 	
-	@OneToMany(mappedBy="cliente")
+	@OneToMany(mappedBy="cliente",cascade=CascadeType.ALL) // cascade serve para apagar todos os  enderecos associados aos clientes
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection // coleção de telefone
